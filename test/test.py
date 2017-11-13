2a1fea9914ac866135aa4ca81b52ce4f79697734
Steven Kang <kjp1314@163.com>
2017-11-13 22:35:30 +0800

    add test.py

---
+++ test/test.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import sys
+import os
+import glob
+import unittest
+
+dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if dirname not in sys.path:
+    sys.path.insert(0, dirname)
+
+
+def test_all():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(test_dir, "test_*.py")
+    files = glob.glob(filename)
+
+    module_names = map(lambda f: os.path.splitext(os.path.basename(f))[0], files)
+    modules = map(__import__, module_names)
+
+    load = unittest.defaultTestLoader.loadTestsFromModule
+    return unittest.TestSuite(map(load, modules))
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest="test_all")
